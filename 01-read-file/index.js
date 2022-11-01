17c7cd8aeadcaf9038b87008d1c3d08157646af9
231globus <231globus@gmail.com>
2022-11-01 23:44:41 +0300

    init: create script to print data from text.txt to console

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+const path = require("path")
+
+const filePath = path.join(__dirname, "text.txt")
+
+const stream = fs.createReadStream(filePath)
+
+stream.on("data", function(chunk) {
+  console.log(chunk.toString())
+})
