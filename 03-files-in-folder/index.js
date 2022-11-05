6cb2574d45ab7d943912037f53e97bb087a7aef8
231globus <231globus@gmail.com>
2022-11-05 14:42:24 +0300

    init: create script to print files in secret-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,34 @@
+const { readdir } = require("fs/promises")
+const fs = require("fs")
+const path = require("path")
+
+const dirPath = path.join(__dirname, "secret-folder")
+
+async function readSecretFolders() {
+  const files = await readdir(dirPath, {withFileTypes: true})
+
+  for(let file of files){
+    if(!file.isDirectory()) {
+      let extName = path.extname(file.name)
+      let fileName = file.name
+
+      const filePath = path.join(dirPath, fileName)
+
+      fs.stat(filePath, (err, stats)=>{
+        if(err) {
+          throw(err)
+        }
+        console.log(
+          fileName.slice(0, fileName.length-extName.length) + " - "
+          + extName.slice(1) + " - " + stats.size + " bites"
+        )
+      })
+    }
+  }
+}
+
+readSecretFolders()
+
+
+
+
